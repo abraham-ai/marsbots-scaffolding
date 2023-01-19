6e2fab64e235551d26c8b75496e72c4f12bf5398
Jonathan Miller <jon@jmill.dev>
2023-01-19 08:09:50 -0800

    Cool

--- discord/bot.py
+++ discord/bot.py
@@ -1,26 +1,10 @@
 import argparse
 import os
 from pathlib import Path
-from dataclasses import dataclass
-from typing import Optional
 
-import discord
-from discord.ext import commands
-from pymongo import MongoClient
 import yaml
 
-
-@dataclass
-class MarsbotMetadata:
-    id: str
-    name: str
-    capabilities: list[str]
-
-    intents: Optional[list[str]] = None
-    command_prefix: Optional[str] = None
-
-
-UNLIKELY_PREFIX = ["438974983724798uyfsduhfksdhfjhksdbfhjgsdyfgsdygfusd"]
+from marsbots.platforms.discord.bot import MarsBot, MarsbotMetadata
 
 
 def parse_manifest(bot_dir) -> MarsbotMetadata:
@@ -30,53 +14,14 @@ def parse_manifest(bot_dir) -> MarsbotMetadata:
     return MarsbotMetadata(**manifest)
 
 
-class MarsBot(commands.Bot):
-    def __init__(self, metadata: MarsbotMetadata) -> None:
-        self.metadata = metadata
-
-        client = MongoClient(os.getenv("MONGODB_URI"))
-        self.db = client["marsbots"]
-
-        intents = discord.Intents.default()
-        self.set_intents(intents)
-        commands.Bot.__init__(
-            self,
-            command_prefix=self.metadata.command_prefix or UNLIKELY_PREFIX,
-            intents=intents,
-        )
-
-    def set_intents(self, intents: discord.Intents) -> None:
-        intents.message_content = True
-        intents.messages = True
-        if self.metadata.intents:
-            if "presence" in self.metadata.intents:
-                intents.presences = True
-            if "members" in self.metadata.intents:
-                intents.members = True
-
-    async def on_ready(self) -> None:
-        print(f"Running {self.metadata.name}...")
-
-    async def on_message(self, message: discord.Message) -> None:
-        if message.author.bot:
-            return
-
-        await self.process_commands(message)
-
-
 def start(
     bot_id: str,
 ) -> None:
-
     bot_dir = Path(__file__).parent / bot_id
     metadata = parse_manifest(bot_dir)
     print("Launching bot...")
 
     bot = MarsBot(metadata)
-    cog_paths = [f"{bot_id}.cogs.{cog.stem}" for cog in bot_dir.glob("cogs/*.py")]
-    for path in cog_paths:
-        ext = bot.load_extension(path)
-        print("Loaded extension:", ext)
     bot.run(os.getenv("DISCORD_TOKEN"))
 
 
